cf0bf23c680276be5f5d28db5075d94b01ab398e
sieiden <sarahieiden@gmail.com>
2021-06-14 22:56:12 -0500

    need to add legend

--- leaflet_step_1/static/js/logic.js
+++ leaflet_step_1/static/js/logic.js
@@ -1,4 +1,5 @@
 function createMap(data){
+  console.log(data);
   let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -12,6 +13,8 @@ function createMap(data){
     let location = [earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]]
     let ecolor = "";
     let depth = earthquake.geometry.coordinates[2];
+    let magnitude = earthquake.properties.mag;
+    let eradius = parseInt(magnitude * 20000)
     if (depth >= 90){
       ecolor = "#FF0000"
     }else if (depth<90 && depth>=70){
@@ -30,7 +33,7 @@ function createMap(data){
       fillOpacity: 0.75,
       color: ecolor,
       fillColor: ecolor,
-      radius: earthquake.geometry.coordinates[2]*1500
+      radius: eradius
     })
     .bindPopup(`<h3>${earthquake.properties.place}<hr> ${Date(earthquake.properties.time)}`))
   });
@@ -41,12 +44,37 @@ function createMap(data){
   let overlayMaps = {
     "Earthquakes": earthquakeLayer
   };
-  let myMap = L.map("mapid", {
+  let myMap = L.map("map", {
     center: [15.5994, -28.6731],
     zoom: 2.5,
     layers: [streetmap, earthquakeLayer]
   });
-  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+  L.control.layers(baseMaps, overlayMaps,{collapsed:false}).addTo(myMap);
+
+  let legend = L.control({position:"bottomright"});
+  legend.onAdd = function(){
+    let div = L.DomUtil.create("div", "info legend");
+    let limits = geojson.options.limits;
+    let colors = geojson.options.colors;
+    var labels = [];
+    var legendInfo = "<h1>Earthquake Depth</h1" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });  
+  
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(myMap);
 }
 
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
