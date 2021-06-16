5e926847753bb1ae19dec9c09fa4762b8135f4f9
sieiden <sarahieiden@gmail.com>
2021-06-16 15:55:18 -0500

    added all of the layers

--- leaflet_step_1/static/js/config.js
+++ leaflet_step_1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoic2llaWRlbiIsImEiOiJja3B4OWxhbncwM2oyMzJuNGUwczRreHJrIn0.QJVwynylYx3Sqktl3zZ8og";
+const API_KEY = "pk.eyJ1Ijoic2llaWRlbiIsImEiOiJja3B4Znk0NHYwMDk2Mm5scjhjd2RsbWJ4In0.hwes2WiM8Rxs1D7ZwKYTPw";
--- leaflet_step_1/static/js/logic.js
+++ leaflet_step_1/static/js/logic.js
@@ -8,6 +8,24 @@ function createMap(data){
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   })
+
+  let satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  })
+
+  let darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+
   let earthquakeMarkers = [];
   data.features.forEach((earthquake)=>{
     let location = [earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]]
@@ -38,7 +56,9 @@ function createMap(data){
     .bindPopup(`<h3>${earthquake.properties.place}<hr> ${Date(earthquake.properties.time)}`))
   });
   let baseMaps = {
-    "Streets": streetmap
+    "Streets": streetmap,
+    "Satellite": satellite,
+    "Dark": darkmap
   };
   let earthquakeLayer = L.layerGroup(earthquakeMarkers);
   let overlayMaps = {
@@ -50,26 +70,28 @@ function createMap(data){
     layers: [streetmap, earthquakeLayer]
   });
   L.control.layers(baseMaps, overlayMaps,{collapsed:false}).addTo(myMap);
+  
+  let breaks = [-10,10,30,50,70,90]
+  let labels = ["-10-10","10-30","30-50","50-70","70-90","90+"]
+
+  function getColor(d){
+    return d > breaks[5] ? "#FF0000":
+    d<breaks[5] && d>=breaks[4] ? "#ff6600":
+    d<breaks[4] && d>=breaks[3] ? "#ffcc00":
+    d<breaks[3] && d>=breaks[2] ? "#99ff00":
+    d<breaks[2] && d>=breaks[1] ? "#33ff00":
+    "#00FF00"
+  }
 
   let legend = L.control({position:"bottomright"});
-  legend.onAdd = function(){
-    let div = L.DomUtil.create("div", "info legend");
-    let limits = geojson.options.limits;
-    let colors = geojson.options.colors;
-    var labels = [];
-    var legendInfo = "<h1>Earthquake Depth</h1" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
-
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });  
-  
-  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  legend.onAdd = function(map){
+    let div = L.DomUTil.create('div','info legend');
+    for (let i = 0; i<breaks.length; i++){
+      div.innterHTML +=
+      '<i style="background:' +
+      getColor(breaks[i]) + '"></i ' +
+      labels[i] + (breaks ? ' ' + '<br>' : '')
+    }
     return div;
   };
 
